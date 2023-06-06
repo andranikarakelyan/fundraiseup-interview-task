a0516ebdd623c8c1be0780e8c3502205bb1166ab
Andranik Arakelyan <andranik.arakelyan.work@gmail.com>
2023-06-06 15:29:29 +0400

    Implemented update/replace events processing

--- src/sync.ts
+++ src/sync.ts
@@ -1,6 +1,6 @@
 import { DbClient } from "./lib/db.client";
 import { anonymizeUsersData } from "./lib/utils";
-import { ChangeStreamInsertDocument, MongoError, ObjectId } from "mongodb";
+import { MongoError, ObjectId } from "mongodb";
 import { TasksBatchExecutor } from "./lib/TasksBatchExecutor";
 import { IUserData } from "./lib/types";
 import { fullReindex } from "./fullReindex";
@@ -17,54 +17,95 @@ async function main() {
   } else {
     console.log("Listening to changes ...");
 
-    const tbe = new TasksBatchExecutor<{
-      user: IUserData;
-      resume_token: ObjectId;
-    }>(async (args) => {
-      try {
-        await DbClient.customers_anonymised_collection.insertMany(
-          anonymizeUsersData(args.map((v) => v.user)),
-          { ordered: false }
-        );
-      } catch (err) {
-        if (err instanceof MongoError && err.code === 11000) {
-          // Do nothing, when there are error about duplicates ( Err E11000 duplicate key error collection )
-        } else throw err;
-      }
-      DbClient.cursors
-        .updateOne(
-          { name: "customers_insert" },
-          {
-            $set: {
-              resume_token: args[args.length - 1].resume_token,
-            },
-          },
-          { upsert: true }
-        )
-        .catch((err) => {
-          console.error("Error during saving resume token", err);
-        });
-    }).start();
+    const tbe = new TasksBatchExecutor<ITaskArg>(execTask).start();
 
     const cursor = await DbClient.cursors.findOne({
-      name: "customers_insert",
+      name: "customers_anonymization ",
     });
 
-    const change_stream = DbClient.customers_collection.watch(
-      [{ $match: { operationType: "insert" } }],
-      {
-        resumeAfter: cursor?.resume_token,
-      }
-    );
+    const change_stream = DbClient.customers_collection.watch([], {
+      resumeAfter: cursor?.resume_token,
+      fullDocument: "updateLookup",
+    });
 
     while (true) {
-      const event =
-        (await change_stream.next()) as ChangeStreamInsertDocument<IUserData>;
-      tbe.addTasks([
-        { user: event.fullDocument, resume_token: event._id as ObjectId },
-      ]);
+      const event = await change_stream.next();
+
+      if (
+        event.operationType === "insert" ||
+        event.operationType === "update" ||
+        event.operationType === "replace"
+      ) {
+        tbe.addTasks([
+          {
+            user: event.fullDocument as IUserData,
+            resume_token: event._id as ObjectId,
+            op_type: event.operationType,
+          },
+        ]);
+      }
     }
   }
+}
+
+interface ITaskArg {
+  user: IUserData;
+  op_type: "insert" | "update" | "replace";
+  resume_token: ObjectId;
+}
+
+async function execTask(args: ITaskArg[]) {
+  try {
+    await DbClient.customers_anonymised_collection.bulkWrite(
+      args.map(({ user, op_type }) => {
+        user = anonymizeUsersData([user])[0];
+
+        if (op_type === "insert") {
+          return {
+            insertOne: {
+              document: user,
+            },
+          };
+        } else if (op_type === "update") {
+          return {
+            updateOne: {
+              filter: { _id: user._id },
+              update: { $set: user },
+              upsert: true,
+            },
+          };
+        } else {
+          return {
+            replaceOne: {
+              filter: { _id: user._id },
+              replacement: user,
+              upsert: true,
+            },
+          };
+        }
+      }),
+      {
+        ordered: false,
+      }
+    );
+  } catch (err) {
+    if (err instanceof MongoError && err.code === 11000) {
+      // Do nothing, when there are error about duplicates ( Err E11000 duplicate key error collection )
+    } else throw err;
+  }
+  DbClient.cursors
+    .updateOne(
+      { name: "customers_anonymization" },
+      {
+        $set: {
+          resume_token: args[args.length - 1].resume_token,
+        },
+      },
+      { upsert: true }
+    )
+    .catch((err) => {
+      console.error("Error during saving resume token", err);
+    });
 }
 
 main().catch((err) => {
